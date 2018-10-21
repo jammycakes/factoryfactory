74759160ad55bab75091fd03ada500e27de898ca
James McKay <code@jamesmckay.net>
2018-10-21 15:29:21 +0100

    Useful debugging info

--- src/FactoryFactory/ServiceDefinition.cs
+++ src/FactoryFactory/ServiceDefinition.cs
@@ -133,5 +133,8 @@ namespace FactoryFactory
         {
             return _identity.GetHashCode();
         }
+
+        public override string ToString() =>
+            $"ServiceDefinition: {this.ServiceType.ToString()}";
     }
 }
