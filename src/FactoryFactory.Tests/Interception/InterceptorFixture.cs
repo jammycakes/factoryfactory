8796ed7405a45088138fc9fddf706acc795efb7f
James McKay <code@jamesmckay.net>
2018-11-08 20:38:54 +0000

    Fixture to check the order in which interceptors are applied.

--- src/FactoryFactory.Tests/Interception/InterceptorFixture.cs
+++ src/FactoryFactory.Tests/Interception/InterceptorFixture.cs
@@ -9,7 +9,7 @@ namespace FactoryFactory.Tests.Interception
     public class InterceptorFixture
     {
         [Fact]
-        public void DecoratedServiceShouldBeDecorated()
+        public void InterceptedServiceShouldBeIntercepted()
         {
             var interceptor = A.Fake<IInterceptor<IServiceWithoutDependencies>>();
             IServiceWithoutDependencies original = null;
@@ -35,7 +35,7 @@ namespace FactoryFactory.Tests.Interception
         }
 
         [Fact]
-        public void SingletonShouldBeDecoratedOnlyOnce()
+        public void SingletonShouldBeInterceptedOnlyOnce()
         {
             var interceptor = A.Fake<IInterceptor<IServiceWithoutDependencies>>();
             A.CallTo(() =>
@@ -57,7 +57,7 @@ namespace FactoryFactory.Tests.Interception
         }
 
         [Fact]
-        public void TransientShouldBeDecoratedOncePerInstance()
+        public void TransientShouldBeInterceptedOncePerInstance()
         {
             var interceptor = A.Fake<IInterceptor<IServiceWithoutDependencies>>();
             A.CallTo(() =>
@@ -76,6 +76,52 @@ namespace FactoryFactory.Tests.Interception
             A.CallTo(() => interceptor.Intercept(
                 A<ServiceRequest>.Ignored,
                 A<Func<IServiceWithoutDependencies>>.Ignored)).MustHaveHappened(2, Times.Exactly);
+        }
+
+        [Fact]
+        public void LastInterceptorRegisteredShouldBeClosestToTheService()
+        {
+            var interceptor1 = A.Fake<IInterceptor<IServiceWithoutDependencies>>();
+            var interceptor2 = A.Fake<IInterceptor<IServiceWithoutDependencies>>();
+            int call1 = 0;
+            int call2 = 0;
+            int postCall1 = 0;
+            int postCall2 = 0;
+            int callCount = 0;
+            int postCallCount = 0;
+            A.CallTo(() =>
+                interceptor1.Intercept(
+                    A<ServiceRequest>.Ignored,
+                    A<Func<IServiceWithoutDependencies>>.Ignored))
+                .ReturnsLazily((ServiceRequest req, Func<IServiceWithoutDependencies> svc) => {
+                    call1 = ++callCount;
+                    var result = svc();
+                    postCall1 = ++postCallCount;
+                    return result;
+                });
+            A.CallTo(() =>
+                interceptor2.Intercept(
+                    A<ServiceRequest>.Ignored,
+                    A<Func<IServiceWithoutDependencies>>.Ignored))
+                .ReturnsLazily((ServiceRequest req, Func<IServiceWithoutDependencies> svc) => {
+                    call2 = ++callCount;
+                    var result = svc();
+                    postCall2 = ++postCallCount;
+                    return result;
+                });
+            var module = new Module();
+            module.Intercept<IServiceWithoutDependencies>().With(interceptor1);
+            module.Intercept<IServiceWithoutDependencies>().With(interceptor2);
+            module.Define<IServiceWithoutDependencies>().As<ServiceWithoutDependencies>()
+                .Transient();
+            var container = Configuration.CreateContainer(module);
+            var service1 = container.GetService<IServiceWithoutDependencies>();
+
+            Assert.Equal(1, postCall1);
+            Assert.Equal(2, postCall2);
+
+            Assert.Equal(2, call1);
+            Assert.Equal(1, call2);
         }
     }
 }
