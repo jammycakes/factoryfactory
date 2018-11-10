96ed7473c228962d900bd7577e48c4c4f23a1743
James McKay <code@jamesmckay.net>
2018-11-10 10:07:43 +0000

    Extension methods to create containers directly from a module.

---
+++ src/FactoryFactory/ModuleAndServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FactoryFactory
+{
+    public static class ModuleAndServiceCollectionExtensions
+    {
+        /// <summary>
+        ///  Creates a root container from this module.
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns></returns>
+        public static IContainer CreateContainer(this Module module)
+        {
+            return new Configuration(module).CreateContainer();
+        }
+
+        /// <summary>
+        ///  Creates a FactoryFactory container from this <see cref="IServiceCollection"/>
+        ///  instance.
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+
+        public static IContainer CreateFactoryFactory(this IServiceCollection collection)
+        {
+            return new Configuration(new Module(collection)).CreateContainer();
+        }
+    }
+}
