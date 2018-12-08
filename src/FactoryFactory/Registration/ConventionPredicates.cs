f9d111ed257f0d8385c9e2cd44e565381c767734
James McKay <code@jamesmckay.net>
2018-12-08 17:53:42 +0000

    Allow multiple assemblies and namespaces to be specified in the predicate.

--- src/FactoryFactory/Registration/ConventionPredicates.cs
+++ src/FactoryFactory/Registration/ConventionPredicates.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using FactoryFactory.Registration.Fluent;
+using FactoryFactory.Util;
 
 namespace FactoryFactory.Registration
 {
     public class ConventionPredicates : IConventionPredicates
     {
         private IList<Predicate<Type>> _predicates = new List<Predicate<Type>>();
+        private IList<Assembly> _assemblies = new List<Assembly>();
+        private IList<(string, bool)> _namespaces = new List<(string, bool)>();
 
         public IConventionPredicates Where(Predicate<Type> predicate)
         {
@@ -15,10 +21,32 @@ namespace FactoryFactory.Registration
             return this;
         }
 
+        public IConventionPredicates FromAssembly(Assembly assembly)
+        {
+            _assemblies.Add(assembly);
+            return this;
+        }
+
+        public IConventionPredicates FromNamespace(string namespacePattern, bool ignoreCase = false)
+        {
+            _namespaces.Add((namespacePattern, ignoreCase));
+            return this;
+        }
+
         public Predicate<Type> ToPredicate()
         {
-            var predicates = new List<Predicate<Type>>(_predicates);
-            return (t => predicates.All(p => p(t)));
+            var predicates = new ReadOnlyCollection<Predicate<Type>>(_predicates);
+            var assemblies = new ReadOnlyCollection<Assembly>(_assemblies);
+            var namespaces = new ReadOnlyCollection<(string, bool)>(_namespaces);
+
+            return requestedType => {
+                if (assemblies.Any() && assemblies.All(a => a != requestedType.Assembly))
+                    return false;
+                if (namespaces.Any() &&
+                    namespaces.All(n => !requestedType.MatchesNamespace(n.Item1, n.Item2)))
+                    return false;
+                return predicates.All(p => p(requestedType));
+            };
         }
     }
 }
--- src/FactoryFactory/Registration/Fluent/IConventionPredicates.cs
+++ src/FactoryFactory/Registration/Fluent/IConventionPredicates.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 
 namespace FactoryFactory.Registration.Fluent
 {
@@ -14,5 +15,30 @@ namespace FactoryFactory.Registration.Fluent
         /// </param>
         /// <returns></returns>
         IConventionPredicates Where(Predicate<Type> predicate);
+
+        /// <summary>
+        ///  Configures a convention to apply to requests for services defined
+        ///  in the given assembly.
+        /// </summary>
+        /// <param name="assembly">
+        ///  The assembly containing the services to be defined by this
+        ///  convention.
+        /// </param>
+        /// <returns></returns>
+        IConventionPredicates FromAssembly(Assembly assembly);
+
+        /// <summary>
+        ///  Configures a convention to apply to requests for services defined
+        ///  in the given namespace.
+        /// </summary>
+        /// <param name="namespacePattern">
+        ///  The namespace to match. Wildcards may be used.
+        /// </param>
+        /// <param name="ignoreCase">
+        ///  Indicates whether or not case should be ignored when matching
+        ///  namespaces.
+        /// </param>
+        /// <returns></returns>
+        IConventionPredicates FromNamespace(string namespacePattern, bool ignoreCase = false);
     }
 }
