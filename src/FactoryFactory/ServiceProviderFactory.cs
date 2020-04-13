2a9426e3e626f29660d5850b2237f01be71fc7b6
James McKay <code@jamesmckay.net>
2020-04-13 19:59:16 +0100

    Bit of formatting tidy up

--- src/FactoryFactory/ServiceProviderFactory.cs
+++ src/FactoryFactory/ServiceProviderFactory.cs
@@ -14,22 +14,25 @@ namespace FactoryFactory
             _options = options;
         }
 
-            Registry IServiceProviderFactory<Registry>.CreateBuilder(IServiceCollection services)
+        Registry IServiceProviderFactory<Registry>.CreateBuilder(IServiceCollection services)
         {
             return new Registry(services);
         }
 
-        IServiceProvider IServiceProviderFactory<Registry>.CreateServiceProvider(Registry containerBuilder)
+        IServiceProvider IServiceProviderFactory<Registry>.CreateServiceProvider
+            (Registry containerBuilder)
         {
             return containerBuilder.CreateContainer(_options);
         }
 
-        IServiceCollection IServiceProviderFactory<IServiceCollection>.CreateBuilder(IServiceCollection services)
+        IServiceCollection IServiceProviderFactory<IServiceCollection>.CreateBuilder
+            (IServiceCollection services)
         {
             return new Registry(services);
         }
 
-        IServiceProvider IServiceProviderFactory<IServiceCollection>.CreateServiceProvider(IServiceCollection containerBuilder)
+        IServiceProvider IServiceProviderFactory<IServiceCollection>.CreateServiceProvider
+            (IServiceCollection containerBuilder)
         {
             return containerBuilder.CreateFactoryFactory(_options);
         }
